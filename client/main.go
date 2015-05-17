f75951b6c1e91d084a25655413348db9e7b77e36
Kezhu Wang <kezhuw@gmail.com>
2015-05-17 21:01:01 +0800

    toy tool to transfer file cross network without encryption

---
+++ client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var args struct {
+	address  string
+	filepath string
+}
+
+func init() {
+	flag.StringVar(&args.address, "address", "", "remote address to connect to")
+	flag.StringVar(&args.filepath, "filepath", "", "file to save")
+}
+
+func parse_args() {
+	flag.Parse()
+
+	if args.address == "" || args.filepath == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+}
+
+func main() {
+	parse_args()
+
+	conn, err := net.Dial("tcp", args.address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	file, err := os.OpenFile(args.filepath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, conn)
+	if err != nil {
+		log.Fatalf("transfer error: %s", err)
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"syscall"
+)
+
+var args struct {
+	address  string
+	filepath string
+}
+
+func init() {
+	flag.StringVar(&args.address, "address", "", "local address to listen on")
+	flag.StringVar(&args.filepath, "filepath", "", "file to transfer")
+}
+
+func parse_args() {
+	flag.Parse()
+
+	if args.address == "" || args.filepath == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+}
+
+func main() {
+	parse_args()
+
+	listener, err := net.Listen("tcp", args.address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open(args.filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		fd, err := syscall.Dup(int(file.Fd()))
+		if err != nil {
+			log.Println(err)
+			conn.Close()
+			break
+		}
+		sourceFile := os.NewFile(uintptr(fd), args.filepath)
+
+		wg.Add(1)
+		go func(conn net.Conn, file *os.File, wg *sync.WaitGroup) {
+			defer conn.Close()
+			_, err := io.Copy(conn, file)
+			if err != nil {
+				log.Printf("transfer to %s error: %s", conn.RemoteAddr(), err)
+			}
+		}(conn, sourceFile, &wg)
+	}
+	wg.Wait()
+}
